894ea8fd007cd3a1bec32617339a00272583dedc
hasanmohdkhan <hasanzian@gmail.com>
2017-07-01 00:12:35 +0530

    Initial Commit

--- app/src/main/java/com/example/hasanzian/contactapp/TabForImages.java
+++ app/src/main/java/com/example/hasanzian/contactapp/TabForImages.java
@@ -5,18 +5,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * Created by hasanZian on 30-06-2017.
  */
 
-public class TabForImages  extends Fragment{
+public class TabForImages extends Fragment{
+
+   public GridView gridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_images, container, false);
+
+
+        gridView=(GridView) rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+
         return rootView;
+
+
+
     }
 
 }
